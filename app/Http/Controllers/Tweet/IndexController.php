57446261f207401030706732b08b5c2766c4590c
yasu-take <atsushi.yasutake@conoc.jp>
2022-06-08 21:17:13 +0900

    update

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use App\Services\TweetService; //TweetServiceのインポート
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request,TweetService $tweetService)
+    {
+        // $tweeetServise = new TweetService(); //TweetServiceのインスタンスを作成
+        $tweets = $tweetService->getTweets(); //つぶやきの一覧を取得
+        return view('tweet.index')
+                ->with('tweets',$tweets);
+    }
+}
